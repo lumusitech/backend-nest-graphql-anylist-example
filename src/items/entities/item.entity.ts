1cceaa9351c72da3b1b90c558e4250b9e35c8814
Luciano Figueroa <lumusitech@gmail.com>
2025-10-29 20:37:16 -0300

    feat: add item entity for graphql and TypeOrm

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,7 +1,25 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Field, Float, ID, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity({ name: 'items' })
 @ObjectType()
 export class Item {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => ID)
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Field(() => String)
+  @Column()
+  name: string;
+
+  @Field(() => Float)
+  @Column()
+  quantity: number;
+
+  @Field(() => String)
+  @Column()
+  quantityUnit: string; // e.g., kg, ml,liters, pieces
+
+  // stores
+  // user
 }
--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ItemsService } from './items.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Item } from './entities/item.entity';
 import { ItemsResolver } from './items.resolver';
+import { ItemsService } from './items.service';
 
 @Module({
   providers: [ItemsResolver, ItemsService],
+  imports: [TypeOrmModule.forFeature([Item])],
 })
 export class ItemsModule {}
