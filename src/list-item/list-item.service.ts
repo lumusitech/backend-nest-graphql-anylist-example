10c2957940c73b1f333afbca7de715ad839ffc11
Luciano Figueroa <lumusitech@gmail.com>
2025-11-24 02:30:52 -0300

    feat: add queries for listItem

--- src/list-item/list-item.service.ts
+++ src/list-item/list-item.service.ts
@@ -1,26 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateListItemInput } from './dto/create-list-item.input';
 import { UpdateListItemInput } from './dto/update-list-item.input';
+import { User } from 'src/users/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ListItem } from './entities/list-item.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ListItemService {
-  create(createListItemInput: CreateListItemInput) {
-    return 'This action adds a new listItem';
+  constructor(
+    @InjectRepository(ListItem)
+    private readonly listItemRepository: Repository<ListItem>,
+  ) { }
+  async create(createListItemInput: CreateListItemInput): Promise<ListItem> {
+    const { listId, itemId, ...rest } = createListItemInput;
+
+    const listItem = this.listItemRepository.create({
+      ...rest,
+      list: { id: listId },
+      item: { id: itemId }
+    });
+
+    return await this.listItemRepository.save(listItem);
   }
 
-  findAll() {
-    return `This action returns all listItem`;
+  async findAll(): Promise<ListItem[]> {
+    return await this.listItemRepository.find({
+      relations: {
+        list: true,
+        item: true,
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} listItem`;
+  async findOne(id: string): Promise<ListItem> {
+    try {
+      return await this.listItemRepository.findOneByOrFail({ id });
+    } catch (error) {
+      throw new NotFoundException(`List item with id ${id} not found`);
+    }
   }
 
-  update(id: number, updateListItemInput: UpdateListItemInput) {
-    return `This action updates a #${id} listItem`;
+  async update(id: string, updateListItemInput: UpdateListItemInput): Promise<ListItem> {
+    const listItem = await this.findOne(id);
+    return await this.listItemRepository.save({ ...listItem, ...updateListItemInput });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} listItem`;
+  async remove(id: string): Promise<ListItem> {
+    const listItem = await this.findOne(id);
+    return await this.listItemRepository.remove(listItem);
   }
 }
